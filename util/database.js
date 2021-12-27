b2abf2d26ebd3640e153cf95f3116a5c48c785a6
Nitish-Kumar-Singh <nitishsinghrajput11291@gmail.com>
2021-12-27 13:28:10 +0530

    Sequelize Node App

---
+++ util/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "root", "Nitish0905@", {
+  dialect: "mysql",
+  host: "localhost",
+});
+module.exports = sequelize;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "Nitish0905@",
+// });
+// module.exports = pool.promise();
